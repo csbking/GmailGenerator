e2e52703ee12e909515ef8d8977ea64c47b6e11e
csbking <alexhuntercsb@gmail.com>
2025-11-30 16:25:46 +0600

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -1,18 +1,1 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # load .env file
-
-real_pass = os.getenv("GM_PASS")
-
-if not real_pass:
-    print("Password not set! (.env file missing)")
-    exit()
-
-user_pass = input("Enter password: ")
-
-if user_pass != real_pass:
-    print("Wrong password!")
-    exit()
-
-print("Access Granted!")
+import gm.py
