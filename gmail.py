eb21ea46ce9ede83e3c68b3d6cbf84b3f78ed1c5
csbking <alexhuntercsb@gmail.com>
2025-11-30 16:06:57 +0600

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -1,1 +1,9 @@
-import gm.zip
+import os
+
+real_pass = os.getenv("GM_PASS")  # সিস্টেম থেকে পাস নেবে
+user_pass = input("Enter password: ")
+
+if user_pass != real_pass:
+    exit()
+
+print("Access Granted")
